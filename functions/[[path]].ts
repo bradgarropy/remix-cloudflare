6037a780037e5688e4fb5e94cde13203fdf7a34b
Brad Garropy <bradgarropy@gmail.com>
2024-06-25 22:03:44 -0500

    test commit.

--- functions/[[path]].ts
+++ functions/[[path]].ts
@@ -6,3 +6,4 @@ import { createPagesFunctionHandler } from "@remix-run/cloudflare-pages";
 import * as build from "../build/server";
 
 export const onRequest = createPagesFunctionHandler({ build });
+// yep
