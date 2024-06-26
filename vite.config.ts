0d79ab02c1aa5754d5f8de7014e1527e02a1e99d
Brad Garropy <bradgarropy@gmail.com>
2024-06-25 21:57:06 -0500

    progress.

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,10 @@
-import { vitePlugin as remix } from "@remix-run/dev";
+import { vitePlugin as remix, cloudflareDevProxyVitePlugin as remixCloudflare } from "@remix-run/dev";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
   plugins: [
+    remixCloudflare(),
     remix({
       future: {
         v3_fetcherPersist: true,
